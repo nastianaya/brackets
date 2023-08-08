48505d184824dcff7f61039edee7c01eca0d8c75
Your Name <you@example.com>
2023-08-08 22:26:36 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const res = [];
+
+  for (const bracket of str) {
+      const [start, end] = bracketsConfig.find(pair => pair.includes(bracket));
+
+      if (bracket === start && !res.includes(start)) {
+        res.push(end);
+          continue;
+      }
+      
+      const stackedBracket = res.pop();
+      if (bracket === stackedBracket) {
+          continue;
+      }
+      return false;
+  }
+
+  return !res.length;
+};
+
+
